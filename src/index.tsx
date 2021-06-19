60135350a17faeac19a7a7196185ec43c9818ee1
Aleksandar Faraj <aleksandarfaraj@gmail.com>
2021-06-19 16:20:20 +0200

    Refactor java-esque ApplicationContainer to more JS-esque name.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { RouteContainer } from './RouteContainer'
-const ApplicationContainer: React.FunctionComponent<{}> = () => (
+const AppContainer: React.FunctionComponent<{}> = () => (
     <RouteContainer></RouteContainer>
 );
-render(<ApplicationContainer />, document.getElementById('todo-app'));
+render(<AppContainer />, document.getElementById('todo-app'));
