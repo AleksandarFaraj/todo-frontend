8d3c93b0c13779db8430d182d0943358b6a68e85
Aleksandar Faraj <aleksandarfaraj@gmail.com>
2021-06-20 16:29:58 +0200

    Battle ref registering vs custom date picker vs textarea.

--- src/views/TodoEditView.tsx
+++ src/views/TodoEditView.tsx
@@ -1,9 +1,15 @@
-import { BackButton } from "~/src/components/util/BackButton"
-import React from "react"
+import React from "react";
 import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
+import useSWR, { mutate } from "swr";
+import { todoPut } from "~/src/api/TodosApi";
 import { CardFormComponent } from "~/src/components/CardForm/CardFormComponent";
+import { CardFormDateTimePicker } from "~/src/components/CardForm/CardFormDatePicker";
 import { CardFormInput } from "~/src/components/CardForm/CardFormInput";
 import { CardFormTextArea } from "~/src/components/CardForm/CardFormTextArea";
+import { BackButton } from "~/src/components/util/BackButton";
+import { fetcher } from "~/src/util/fetcher";
+import { CardFormTypeSelector } from "../components/CardForm/CardFormTypeSelector";
 
 export const TodoEditView: React.FunctionComponent<{ todo?: Todo }> = ({ todo = { title: "Fix styling after this", description: "what" } }) => {
 
@@ -20,15 +26,16 @@ export const TodoEditView: React.FunctionComponent<{ todo?: Todo }> = ({ todo = 
                 <button type="submit">Done</button>
             </div>
             <CardFormComponent>
-                <CardFormInput refRegister={() => register("title")} placeholder="Todo" />
-                <CardFormTextArea refRegister={() => register("description")} placeholder="Notes" />
+                <CardFormInput refRegister={() => register("title")} placeholder="Todo" defaultValue={data.title} />
+                <CardFormTextArea refRegister={() => register("description")} placeholder="Notes" defaultValue={data.description} />
+            </CardFormComponent>
+            <CardFormComponent>
+                <CardFormDateTimePicker control={control} name="due_date" defaultValue={new Date(data.due_date)}></CardFormDateTimePicker>
             </CardFormComponent>
             <div>
-                Type
+                <CardFormTypeSelector refRegister={() => register("todo_type")} placeholder="Todo" defaultValue={data.title} ></CardFormTypeSelector>
             </div>
-            <div>
-                Do date
-            </div>
+
 
         </form>
         <button>Delete</button>
