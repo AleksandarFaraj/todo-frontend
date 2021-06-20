d605055f6749c11d41b4623b15d42cc9dc4d2073
Aleksandar Faraj <aleksandarfaraj@gmail.com>
2021-06-20 19:19:26 +0200

    Add fetch functions for create, update, delete

--- src/api/TodosApi.tsx
+++ src/api/TodosApi.tsx
@@ -1,12 +1,27 @@
 const defaultHeaders = {
     'Content-Type': 'application/json'
 };
-
-export const todoPut = async (todo: Todo): Promise<Todos> => {
-    const response = await fetch(`http://localhost:8080/todos/${todo.id}`, {
+export const todoAdd = async (todo: Partial<Todo>): Promise<Todo> => {
+    const response = await fetch(`http://localhost:8080/todo/${todo.id}`, {
+        method: "POST",
+        headers: defaultHeaders,
+        body: JSON.stringify(todo)
+    })
+    return response.json();
+}
+export const todoPut = async (todo: Todo): Promise<Todo> => {
+    const response = await fetch(`http://localhost:8080/todo/${todo.id}`, {
         method: "PUT",
         headers: defaultHeaders,
         body: JSON.stringify(todo)
+    })
+    return response.json();
+}
+
+export const todoDelete = async (todo: Todo): Promise<Todo> => {
+    const response = await fetch(`http://localhost:8080/todo/${todo.id}`, {
+        method: "DELETE",
+        headers: defaultHeaders,
     })
     return response.json();
 }
