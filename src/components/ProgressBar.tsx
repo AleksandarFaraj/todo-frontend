bbe2ceed2666b41896774e4ebdd7d6235bca4f82
Aleksandar Faraj <aleksandarfaraj@gmail.com>
2021-06-20 18:34:50 +0200

    add progress bar animation and transition. Worked without hassle actually. Didn't expect react and tailwind to work well together.

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const ProgressBar: React.FunctionComponent<{ finished?: number }> = ({ finished = 30 }) => (
+export const ProgressBar: React.FunctionComponent<{ finished?: number }> = ({ finished = 0.3 }) => (
     <div className="w-full my-4">
         <div className="h-2 flex rounded bg-pink-200">
-            <div style={{ width: `${finished}%` }} className="bg-pink-500"></div>
+            <div style={{ width: `${finished * 100}%` }} className={`transition-all ease-in-out duration-300 rounded bg-pink-500 ${finished < 1 ? "animate-pulse" : ""}`} ></div>
         </div>
-    </div>
+    </div >
 )
