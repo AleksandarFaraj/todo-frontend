21780613d1d362fa38e813294891eba3f3dd3310
Aleksandar Faraj <aleksandarfaraj@gmail.com>
2021-06-20 10:49:07 +0200

    Adding Todo context

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { render } from 'react-dom';
 import { AppStyleComponent } from './components/AppStyleComponent';
 import { RouteContainer } from './RouteContainer'
 import "/src/tailwind.css";
+
+export const TodosContext = createContext({ todos: [] })
+
 const AppContainer: React.FunctionComponent<{}> = () => (
     <AppStyleComponent>
         <RouteContainer></RouteContainer>
