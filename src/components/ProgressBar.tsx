94fb7aa64be0e9dd143ed4cd78e16b5d924fde6c
Aleksandar Faraj <aleksandarfaraj@gmail.com>
2021-06-20 19:15:45 +0200

    Progress bar refactor to have value prop instead of context specific finished or completion. Need to learn git cherry pick.

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const ProgressBar: React.FunctionComponent<{ finished?: number }> = ({ finished = 0.3 }) => (
+export const ProgressBar: React.FunctionComponent<{ value?: number }> = ({ value: value = 0.3 }) => (
     <div className="w-full my-4">
         <div className="h-2 flex rounded bg-pink-200">
-            <div style={{ width: `${finished * 100}%` }} className={`transition-all ease-in-out duration-300 rounded bg-pink-500 ${finished < 1 ? "animate-pulse" : ""}`} ></div>
+            <div style={{ width: `${value * 100}%` }} className={`transition-all ease-in-out duration-300 rounded bg-pink-500 ${value < 1 ? "animate-pulse" : ""}`} ></div>
         </div>
     </div >
 )
--- src/views/overview/components/dashboard/DashboardProgressComponent.tsx
+++ src/views/overview/components/dashboard/DashboardProgressComponent.tsx
@@ -14,7 +14,7 @@ export const DashboardProgressComponent: React.FunctionComponent<{}> = () => {
         <div className="flex flex-col items-center mt-16 mx-8" >
             <div className="text-5xl m-4">😊</div>
             <div className="text-xl m-4">Lets get started</div>
-            {data && data.length > 0 ? <ProgressBar finished={data.reduce((p, todo) => todo.status ? p+1 : p, 0) / data.length} /> : <div>There are no items here! Perhaps you should add some?</div>}
+            {data && data.length > 0 ? <ProgressBar value={data.reduce((p, todo) => todo.status ? p + 1 : p, 0) / data.length} /> : <div>There are no items here! Perhaps you should add some?</div>}
 
         </div >
     )
